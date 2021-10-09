d69eed5a7a00be60ca30fe3e89edb77a95ac93c6
Calandrinon <georgesut@yahoo.com>
2021-10-10 01:07:37 +0300

    Finish the producer/consumer problem
    Additionally, instead of having the consumer being stuck in a waiting state indefinitely (when the whole sequence of operations ends), the wait_for method is used, which will cause a 3-seconds timeout if the buffer is empty.

--- Labs/Lab-2/Producer-Consumer-Problem/generate_vectors.cpp
+++ Labs/Lab-2/Producer-Consumer-Problem/generate_vectors.cpp
@@ -9,27 +9,14 @@
 #include <fstream>
 
 int main() {
+    std::ofstream out("vectors.txt");
     unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
     auto randomEngine = std::default_random_engine(seed);
-    unsigned long numberOfElements = randomEngine() % 1000000;
-    std::vector<unsigned long> firstVector, secondVector;
-
-    for (int i = 0; i < numberOfElements; i++) {
-        auto numberForTheFirstVector = randomEngine(),
-             numberForTheSecondVector = randomEngine();
-
-        firstVector.push_back(numberForTheFirstVector);
-        secondVector.push_back(numberForTheSecondVector);
-    }
-
-    std::ofstream out("vectors.txt");
+    int numberOfElements = 10000;
+    out << numberOfElements << "\n";
 
     for (int i = 0; i < numberOfElements; i++)
-        out << firstVector[i] << " ";
-    out << "\n";
-
-    for (int i = 0; i < numberOfElements; i++)
-        out << secondVector[i] << " ";
+        out << 100 << " " << 100 << "\n";
 
     return 0;
 }
--- Labs/Lab-2/Producer-Consumer-Problem/main.cpp
+++ Labs/Lab-2/Producer-Consumer-Problem/main.cpp
@@ -2,29 +2,57 @@
 #include <thread>
 #include <vector>
 #include <fstream>
+#include <cstdlib>
+#include <cstdio>
+#include <mutex>
+#include <chrono>
+#include <condition_variable>
 
-std::ifstream in("");
-
+const int maximumBufferSize = 100;
+std::ifstream in("../vectors.txt");
 std::vector<int> productsBuffer;
-std::atomic<int> atomicSum;
+int sum = 0;
+std::mutex mutex;
+std::condition_variable conditionVariable;
 
-std::pair<std::vector<int>, std::vector<int>> readVectors() {
-    std::vector<int> firstVector, secondVector;
-
-    // read from vectors.txt
-
-    return std::make_pair(firstVector, secondVector);
-}
-
-void productsProducer() {
-    // produce products
+void productsProducer(int* numberOfElements) {
+    while (*numberOfElements > 0) {
+        std::unique_lock uniqueLock(mutex);
+        conditionVariable.wait(uniqueLock, []() { return productsBuffer.size() < maximumBufferSize; });
+        std::cout << "Producer's turn...\n";
+        int firstValue, secondValue, currentNumberOfElements;
+        in >> firstValue >> secondValue;
+        productsBuffer.push_back(firstValue*secondValue);
+        currentNumberOfElements = *numberOfElements;
+        *numberOfElements = currentNumberOfElements - 1;
+        uniqueLock.unlock();
+        conditionVariable.notify_one();
+    }
 }
 
 void sumConsumer() {
-    // compute the sum of the products (dot product of the two vectors)
+    while (true) {
+        std::unique_lock uniqueLock(mutex);
+        std::cout << "Consumer's turn...\n";
+        conditionVariable.wait_for(uniqueLock, std::chrono::milliseconds(3000), []() {return !productsBuffer.empty();});
+        if (productsBuffer.empty())
+            return;
+        sum += productsBuffer.back();
+        productsBuffer.pop_back();
+        uniqueLock.unlock();
+        conditionVariable.notify_one();
+        std::cout << "Sum: " << sum <<"\n";
+    }
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int numberOfElements;
+    in >> numberOfElements;
+    std::thread producerThread(productsProducer, &numberOfElements);
+    std::thread consumerThread(sumConsumer);
+    producerThread.join();
+    consumerThread.join();
+
+    std::cout << sum << "\n";
     return 0;
 }
