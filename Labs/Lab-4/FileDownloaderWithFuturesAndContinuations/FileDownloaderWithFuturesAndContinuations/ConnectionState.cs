be32b3d723621a643d81ce66c43c0555ba506a91
Calandrinon <georgesut@yahoo.com>
2021-11-16 06:59:26 +0200

    Finish callback-version of the program

--- Labs/Lab-4/FileDownloaderWithFuturesAndContinuations/FileDownloaderWithFuturesAndContinuations/ConnectionState.cs
+++ Labs/Lab-4/FileDownloaderWithFuturesAndContinuations/FileDownloaderWithFuturesAndContinuations/ConnectionState.cs
@@ -6,52 +6,20 @@ namespace FileDownloaderWithFuturesAndContinuations
 {
     public class ConnectionState
     {
-        public int BytesRemaining
-        {
-            get => bytesRemaining;
-            set => bytesRemaining = value;
-        }
-
-        public IPAddress Address
-        {
-            get => address;
-            set => address = value ?? throw new ArgumentNullException(nameof(value));
-        }
-
-        public byte[] Buffer
-        {
-            get => buffer;
-            set => buffer = value ?? throw new ArgumentNullException(nameof(value));
-        }
-
-        public Socket Socket
-        {
-            get => socket;
-            set => socket = value ?? throw new ArgumentNullException(nameof(value));
-        }
-
-        public string Host
-        {
-            get => host;
-            set => host = value ?? throw new ArgumentNullException(nameof(value));
-        }
-
-        public string Resource
-        {
-            get => resource;
-            set => resource = value ?? throw new ArgumentNullException(nameof(value));
-        }
-
-        public string Response
-        {
-            get => response;
-            set => response = value ?? throw new ArgumentNullException(nameof(value));
-        }
-
+        public int BytesRemaining { get => bytesRemaining; set => bytesRemaining = value; }
+        public IPAddress Address { get => address; set => address = value ?? throw new ArgumentNullException(nameof(value)); }
+        public byte[] Buffer { get => buffer; set => buffer = value ?? throw new ArgumentNullException(nameof(value)); }
+        public Socket Socket { get => socket; set => socket = value ?? throw new ArgumentNullException(nameof(value)); }
+        public string Host { get => host; set => host = value ?? throw new ArgumentNullException(nameof(value)); }
+        public string Resource { get => resource; set => resource = value ?? throw new ArgumentNullException(nameof(value)); }
+        public string Response {get => response; set => response = value ?? throw new ArgumentNullException(nameof(value)); }
+        public int ContentLength { get => contentLength; set => contentLength = value; }
+        public string OriginalUrl { get => originalUrl; set => originalUrl = value; }
+        
         IPAddress address;
         byte[] buffer;
         Socket socket;
-        String host, resource, response;
-        int bytesRemaining;
+        String host, resource, response, originalUrl;
+        int bytesRemaining, contentLength;
     }
 }
--- Labs/Lab-4/FileDownloaderWithFuturesAndContinuations/FileDownloaderWithFuturesAndContinuations/RequestResponseHandler.cs
+++ Labs/Lab-4/FileDownloaderWithFuturesAndContinuations/FileDownloaderWithFuturesAndContinuations/RequestResponseHandler.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace FileDownloaderWithFuturesAndContinuations
 {
@@ -14,7 +15,10 @@ namespace FileDownloaderWithFuturesAndContinuations
         public RequestResponseHandler(List<String> listOfUrls)
         {
             this.listOfUrls = listOfUrls;
-            this.MakeRequest(listOfUrls[0]);
+            foreach (String url in listOfUrls)
+            {
+                this.MakeRequest(url);
+            }
         }
 
         private static Tuple<String, int> parseResponse(String response)
@@ -22,17 +26,16 @@ namespace FileDownloaderWithFuturesAndContinuations
             String header = response.Substring(0, response.IndexOf("<"));
             int contentLengthStartIndex = header.IndexOf("Content-Length:") + "Content-Length:".Length;
             int contentLengthEndIndex = header.IndexOf("\n");
-            String contextLengthSubstring = response.Substring(contentLengthStartIndex, contentLengthEndIndex);
+            String contentLengthSubstring = response.Substring(contentLengthStartIndex, contentLengthEndIndex);
             Regex regexPattern = new Regex(@"[0-9]+");
-            String contextLengthValue = Regex.Match(contextLengthSubstring, @"\d+").Value;
-            return new Tuple<String, int>(header, int.Parse(contextLengthValue));
+            String contentLengthValue = Regex.Match(contentLengthSubstring, @"\d+").Value;
+            return new Tuple<String, int>(header, int.Parse(contentLengthValue));
         }
 
         private void RequestSendingCallback(IAsyncResult asyncResult)
         {
             ConnectionState connectionState = (ConnectionState) asyncResult.AsyncState;
             connectionState.Socket.EndSend(asyncResult);
-            Console.WriteLine("Sent the message.");
 
             connectionState.Socket.BeginReceive(connectionState.Buffer, 0, connectionState.Buffer.Length,
                 0, ResponseCallback, connectionState);
@@ -40,46 +43,13 @@ namespace FileDownloaderWithFuturesAndContinuations
         
         private void ResponseCallback(IAsyncResult asyncResult)
         {
-            Console.WriteLine("In the ResponseCallback...");
             ConnectionState connectionState = (ConnectionState) asyncResult.AsyncState;
-
-            try
-            {
-                int numberOfBytesReceived = connectionState.Socket.EndReceive(asyncResult);
-                String response = Encoding.ASCII.GetString(connectionState.Buffer, 0, numberOfBytesReceived);
-                connectionState.Response += response;
-                
-                Console.WriteLine("Accumulated response:");
-                Console.WriteLine(connectionState.Response);
-
-                if (connectionState.Response.Contains("\r\n\r\n"))
-                {
-                    Console.WriteLine("========= The response contains rnrn pattern =========");
-                    (String responseHeader, int contentLength) = parseResponse(connectionState.Response);
-
-                    int patternIndex = connectionState.Response.IndexOf("\r\n\r\n");
-                    Console.WriteLine("RNRN pattern index:");
-                    Console.WriteLine(patternIndex);
-                    Console.WriteLine("===================================================");
-                    Console.WriteLine("Response header:");
-                    Console.WriteLine(responseHeader);
-                    Console.WriteLine("Obtained content length: {0}", contentLength);
-                    Console.WriteLine("===================================================");
-                    Console.WriteLine("Currently receiving the response...");
-                    Console.WriteLine("This is the received message: {0}", response);
-                    Console.WriteLine("Received the response: {0} bytes", numberOfBytesReceived);
-                }
-                else
-                {
-                    Console.WriteLine("========= The response doesn't contain rnrn pattern yet =========");
-                    connectionState.Socket.BeginReceive(connectionState.Buffer, 0, connectionState.Buffer.Length,
-                0, ResponseCallback, connectionState);
-                }
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine("An exception has occured: {0}", exception.ToString());
-            }
+            int numberOfBytesReceived = connectionState.Socket.EndReceive(asyncResult);
+            connectionState.Response = Encoding.ASCII.GetString(connectionState.Buffer, 0, numberOfBytesReceived);
+            (String responseHeader, int contentLength) = parseResponse(connectionState.Response);
+            connectionState.Socket.Shutdown(SocketShutdown.Both);
+            connectionState.Socket.Close();
+            Console.WriteLine("URL: {0}\nContent-Length: {1}\n\n", connectionState.OriginalUrl, contentLength);
         }
 
         private void HandleConnection(IAsyncResult asyncResult)
@@ -89,7 +59,6 @@ namespace FileDownloaderWithFuturesAndContinuations
             connectionState.Socket.EndConnect(asyncResult);
 
             String httpHeader = String.Format("GET {0}/ HTTP/1.1\r\nHost: {1}\r\nContent-Length: 0\r\n\r\n", connectionState.Resource, connectionState.Host);
-            Console.WriteLine(httpHeader);
 
             byte[] httpHeaderBuffer = Encoding.ASCII.GetBytes(httpHeader);
             connectionState.Socket.BeginSend(httpHeaderBuffer, 0, httpHeaderBuffer.Length,
@@ -105,15 +74,15 @@ namespace FileDownloaderWithFuturesAndContinuations
             IPHostEntry dnsResolvedAddress = Dns.GetHostEntry(connectionState.Host);
             List<IPAddress> addresses = dnsResolvedAddress.AddressList.ToList();
             IPEndPoint endpoint = new IPEndPoint(dnsResolvedAddress.AddressList[0], 80);
-            connectionState.Buffer = new byte[128];
+            connectionState.OriginalUrl = url;
+            connectionState.Buffer = new byte[1024];
             connectionState.Socket = new Socket(endpoint.Address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             connectionState.Host = urlTokens[0];
             connectionState.Resource = url.Substring(url.IndexOf("/"));
             connectionState.Address = addresses[0];
             
             IAsyncResult asyncResult = connectionState.Socket.BeginConnect(endpoint, HandleConnection, connectionState);
-            while (!asyncResult.IsCompleted) { }
-            Console.WriteLine("Connection has been established.");
+            System.Threading.Thread.Sleep(100);
         }
 
     }
