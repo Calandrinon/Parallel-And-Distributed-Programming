c309d8c2e580c4214d699f4a1d8fd74e0c537c63
Calandrinon <georgesut@yahoo.com>
2021-11-16 09:17:45 +0200

    Add documentation for Lab-4

--- Labs/Lab-4/HttpHeaderReader-Futures-And-Continuations/HttpHeaderReader-Futures-And-Continuations/HttpHeaderReader.cs
+++ Labs/Lab-4/HttpHeaderReader-Futures-And-Continuations/HttpHeaderReader-Futures-And-Continuations/HttpHeaderReader.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
@@ -23,13 +24,19 @@ namespace HttpHeaderReaderWithFuturesAndContinuations
         
         public static void Main(String[] args)
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             // The filepath contains the .. pattern for backwards directory navigation, because Jetbrains Rider 
             // apparently reads files from the bin/ directory where the compiled executables are saved and placing the
             // URL file there doesn't make much sense
             List<String> urls = ReadUrlList("../../../URLs.txt");
-            //RequestResponseHandlerWithCallbacks requestResponseHandlerWithCallbacks = new RequestResponseHandlerWithCallbacks(urls);
-            //RequestResponseHandlerWithTasks requestResponseHandlerWithTasks = new RequestResponseHandlerWithTasks(urls);
-            AsynchronousRequestResponseHandler asynchronousRequestResponseHandler = new AsynchronousRequestResponseHandler(urls);
+            RequestResponseHandlerWithCallbacks requestResponseHandlerWithCallbacks = new RequestResponseHandlerWithCallbacks(urls);
+            // RequestResponseHandlerWithTasks requestResponseHandlerWithTasks = new RequestResponseHandlerWithTasks(urls);
+            // AsynchronousRequestResponseHandler asynchronousRequestResponseHandler = new AsynchronousRequestResponseHandler(urls);
+            stopwatch.Stop();            
+            
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("Elapsed time: {0} milliseconds", stopwatch.ElapsedMilliseconds);
         }
     }
 }
